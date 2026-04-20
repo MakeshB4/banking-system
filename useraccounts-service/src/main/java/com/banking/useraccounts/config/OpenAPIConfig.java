3290b4a16851786dbf200cf4f6462a2a9d794b99
unknown <makeshb@ymail.com>
2026-04-20 22:35:32 +0530

    useraccounts changes base config

---
+++ useraccounts-service/src/main/java/com/banking/useraccounts/config/OpenAPIConfig.java
@@ -0,0 +1,37 @@
+package com.banking.useraccounts.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Swagger/OpenAPI configuration
+ */
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI notificationServiceAPI() {
+        Contact contact = new Contact();
+        contact.setEmail("support@banking.com");
+        contact.setName("Banking UserAccounts Team");
+        contact.setUrl("https://www.banking.com");
+
+        License mitLicense = new License()
+                .name("MIT License")
+                .url("https://choosealicense.com/licenses/mit/");
+
+        Info info = new Info()
+                .title("Banking UserAccounts Service API")
+                .version("1.0")
+                .contact(contact)
+                .description("API for User and Accounts Creation")
+                .termsOfService("https://www.banking.com/terms")
+                .license(mitLicense);
+
+        return new OpenAPI().info(info);
+    }
+}
