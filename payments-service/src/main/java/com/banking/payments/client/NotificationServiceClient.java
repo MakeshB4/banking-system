2857c49ec20c869a6f6f5fda92622d2671f9b451
unknown <makeshb@ymail.com>
2026-05-01 15:57:25 +0530

    Testing changes

---
+++ payments-service/src/main/java/com/banking/payments/client/NotificationServiceClient.java
@@ -0,0 +1,49 @@
+package com.banking.payments.client;
+
+import com.banking.payments.dto.AccountBalanceResponse;
+import com.banking.payments.dto.PaymentRequest;
+import com.banking.payments.entity.Payment;
+import com.banking.payments.exceptions.AccountNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationServiceClient {
+
+    private final RestTemplate restTemplate;
+
+    private static final String NOTIFICATION_SERVICE_URL = "http://localhost:8083/notification-service/api/notifications/send";
+
+    public void sendNotification(PaymentRequest paymentRequest,Long transactionId) {
+        AccountBalanceResponse response;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> notificationPayload = new HashMap<>();
+        notificationPayload.put("userId", paymentRequest.getSenderName());
+        notificationPayload.put("type", "EMAIL");
+        notificationPayload.put("recipient", paymentRequest.getEmail());
+        notificationPayload.put("subject", "Payment Notification");
+        notificationPayload.put("message", "Payment of " + paymentRequest.getAmount() + " " + paymentRequest.getCurrency() + " has been initiated with"+transactionId);
+        notificationPayload.put("createdBy", paymentRequest.getSenderName());
+
+        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(notificationPayload, headers);
+
+        try {
+            restTemplate.postForEntity(NOTIFICATION_SERVICE_URL, requestEntity, String.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error while sending notification: " + paymentRequest.getSenderName()+" "+paymentRequest.getPaymentType()+" " +paymentRequest.getEmail());
+        }
+    }
+}
